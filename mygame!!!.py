fcba1fed93e3f7f289e846c95dbec063031ec402
emailmeydrive <emailme.ydrive@gmail.com>
2022-02-02 14:37:41 +0200

    liel gayzi

--- mygame!!!.py
+++ mygame!!!.py
@@ -1,5 +1,8 @@
+from random import random
+from turtle import delay
 import pygame
 import time
+import pygame.freetype
 from pygame import mixer
 
 # screen size 
@@ -11,6 +14,7 @@ WINDOW_SIZE = (WINDOW_W, WINDOW_H)
 pygame.init()
 mixer.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
 pygame.display.set_caption("ziv - game ")
 background_music = pygame.mixer.Sound("mis.mp3")
 pygame.mixer.music.load("mis.mp3")
@@ -34,21 +38,24 @@ x_step = 10
 laser_list = []
 play = True
 
-
+sc = 0
 def print_lasers():
+  global sc , circle_x
   for i in range(len(laser_list)):
-    l = laser_list[i]
-    screen.blit(laser_image,(l[0],l[1]))
-    laser_list[i] = [l[0],l[1]-30]
-
-  if len(laser_list) > 0 and laser_list[0][1] < 0:
-    laser_list.remove(laser_list[0])
-
-
+    laser = laser_list[i]
+    screen.blit(laser_image,(laser[0],laser[1]))
+    laser_list[i] = [laser[0],laser[1]-30]
+    if is_laser_hit(laser):
+      print("hit")
+      sc +=1
+      circle_x = 0
+def is_laser_hit(laser_pos):
+  return abs(laser_pos[0]-circle_x) <30 and abs(laser_pos[1]-circle_y) <30 
 
 
 while play:
   screen.blit(bk_image,(0,0))
+  textsurface = myfont.render("score :"+str(sc), False, (255, 255, 255))
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       play = False
@@ -69,6 +76,7 @@ while play:
   screen.blit(ship_image,(ship_x,ship_y))
   pygame.draw.circle(screen,(255,255,255),(circle_x , circle_y),10)
   print_lasers()
+  screen.blit(textsurface,(0,0))
   circle_x +=circle_x_step
   if circle_x > WINDOW_W:
     circle_x_step = -10
